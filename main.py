ee7b4d611b87ff230f8770ebd3b6e70642017bf7
R4DM4N <47617496+R4DM4N@users.noreply.github.com>
2025-10-27 18:29:33 +1000

    char_count(book) function to stats

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from stats import get_num_words
+from stats import char_count
 
 def main():
     book_path = "./books/frankenstein.txt"
     book_content = get_book_text(book_path)
+    letter_dict ={}
+
+    print(f"Found {get_num_words(book_content)} total words\n")
     
-    print(f"Found {get_num_words(book_content)} total words")
+    letter_dict = char_count(book_content)
+    print(f"Number characters in the book are: {(letter_dict)}")
 
 def get_book_text(path_to_file):
     file_contents = ""
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
+
+
+
+
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,17 @@
 def get_num_words(book):
     word_list = book.split()
     return len(word_list)
+
+def char_count(book):
+    word_list = book.split()
+    char_dict = {}
+
+    for word in word_list:
+        
+        for char in word.lower():
+            
+            if char in char_dict:
+                char_dict[char] = char_dict[char] + 1
+            else:
+                char_dict[char] = 1
+    return char_dict
