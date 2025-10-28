cbea75f1ba6ced16b9a8686341217c5e775755a8
R4DM4N <47617496+R4DM4N@users.noreply.github.com>
2025-10-28 15:38:07 +1000

    Working with boots on the report

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from stats import get_num_words, char_count
+from stats import get_num_words, char_count#, dict_report
 
 def main():
     book_path = "./books/frankenstein.txt"
     book_content = get_book_text(book_path)
     letter_dict ={}
 
-    print(f"Found {get_num_words(book_content)} total words\n")
+    #print(f"Found {get_num_words(book_content)} total words\n")
     
-    letter_dict = char_count(book_content)
+    #letter_dict = dict_report(char_count(book_content))
+    
     print(f"Number characters in the book are: {letter_dict}")
+    
 
 def get_book_text(path_to_file):
     file_contents = ""
@@ -16,7 +18,12 @@ def get_book_text(path_to_file):
         file_contents = f.read()
     return file_contents
 
-
+#Each dictionary should have two key-value pairs: 
+# one for the character itself and one for that character's count
+# (e.g. {"char": "b", "num": 4868}).
+def dict_report(dict_in):
+    dict_in.sort(reverse= False,key=sort_on)
+    print(sorted)
 
 
 
--- stats.py
+++ stats.py
@@ -15,3 +15,29 @@ def char_count(book):
             else:
                 char_dict[char] = 1
     return char_dict
+
+def book_report(book):
+    title ="============ BOOKBOT ============"
+    heading1 ="----------- Word Count ----------"
+    heading2 ="--------- Character Count -------"
+    footer ="============= END ==============="
+
+#Each dictionary should have two key-value pairs: 
+# one for the character itself and one for that character's count
+# (e.g. {"char": "b", "num": 4868}).
+def dict_report(dict_in):
+    dict_in.sort(reverse= False,key=sort_on)
+    print(sorted)
+
+def sorted_list(input):
+    ret_dict = {}
+    
+    new_list = {"char": list(input.keys()), "num": list(input.values())}
+    ls = []
+    for character, number  in input.items():
+        ls.append({"char": character, "num": number})
+    ls.sort(key=sort_on, reverse=True) # We want the largest count to be first
+        
+#key sort helper functher
+def sort_on(item):
+    return item["num"]
