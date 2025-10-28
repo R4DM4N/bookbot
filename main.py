2bc3e9030c7d8e49b1d6ec4d89991152c00d3bbd
R4DM4N <47617496+R4DM4N@users.noreply.github.com>
2025-10-28 16:13:20 +1000

    Refactored sorted_list to stats.py from main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, char_count#, sorted_list, sort_on#, dict_report
+from stats import get_num_words, char_count, sorted_list
 
 def main():
     book_path = "./books/frankenstein.txt"
@@ -15,31 +15,22 @@ def main():
     sorted_characters=sorted_list(letter_dict)
     print(sorted_characters)
 
+    title ="============ BOOKBOT ============"
+    heading1 ="----------- Word Count ----------"
+    heading2 ="--------- Character Count -------"
+    footer ="============= END ==============="
+
 def get_book_text(path_to_file):
     file_contents = ""
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
-#Each dictionary should have two key-value pairs: 
-# one for the character itself and one for that character's count
-# (e.g. {"char": "b", "num": 4868}).
-def dict_report(dict_in):
-    dict_in.sort(reverse= False,key=sort_on)
-    print(sorted)
-
-def sorted_list(input):
-    ret_dict = {}
-    #new_list = {"char": list(input.keys()), "num": list(input.values())}
-    ls = []
-    for character, number  in input.items():
-        ls.append({"char": character, "num": number})
-    ls.sort(key=sort_on, reverse=True) # We want the largest count to be first
-    print(ls)    
-
-#key sort helper functher
-def sort_on(item):
-    return item["num"]
+def book_report(book):
+    title ="============ BOOKBOT ============"
+    heading1 ="----------- Word Count ----------"
+    heading2 ="--------- Character Count -------"
+    footer ="============= END ==============="
 
 
 
